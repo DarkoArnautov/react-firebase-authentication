09bb10690d8249f5daad30f1d0a2602f76f4bd74
Darko <darkoservantes@outlook.com>
2018-12-21 13:29:58 +0300

    Router, Navigation added

---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PasswordForgetPage = () => (
+	<div>
+		PasswordForget Page
+	</div>
+);
+
+export default PasswordForgetPage;
---
+++ src/components/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignInPage = () => (
+	<div>
+		SignIn page
+	</div>
+);
+
+export default SignInPage;
