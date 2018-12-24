156f1eea5dc06a20bc4288209b94b94b81d54f37
Darko <darkoservantes@outlook.com>
2018-12-24 15:02:50 +0300

    SignUp functionality add

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,7 +2,7 @@ import app from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DOMAIN_URL,
     projectId: process.env.REACT_APP_PROJECT_ID,
@@ -20,11 +20,11 @@ class Firebase {
   // *** Auth API ***
 
   doCreateUserWithEmailAndPassword = (email, password) =>(
-    this.auth.createUserWithEmailAndPassword(email, password);
+    this.auth.createUserWithEmailAndPassword(email, password)
   )
 
   doSignInWithEmailAndPassword = (email, password) =>(
-    this.auth.signInWithEmailAndPassword(email, password);
+    this.auth.signInWithEmailAndPassword(email, password)
   )
 
   doSignOut = () => this.auth.signOut();
